b8d92ee513284d84fa1306268dbd08865e947fb5
C4rb0rg <139198968+Carbreg@users.noreply.github.com>
2023-07-13 21:15:17 +0530

    Changed the main font to VT23 and made some changes to the UI

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 document.getElementById("generate-button").addEventListener("click", function() {
-    var image = document.getElementById("image");
+  var image = document.getElementById("image");
+
+  // Logic to generate glitched image
+  // ...
+
+  // Set glitched image source
+  image.src = "My-photo-for-the-socials-_1_-reverb.png";
+});
+
+document.getElementById("send-button").addEventListener("click", function() {
+  var emailBox = document.getElementById("email-box");
+  emailBox.classList.toggle("show-email-box");
+});
+
+document.getElementById("email-input").addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) { // Enter key pressed
+    sendEmail();
+  }
+});
+
+function sendEmail() {
+  var emailInput = document.getElementById("email-input").value;
+
+  // Logic to send the glitched image to the specified email
+  // ...
   
-    // Logic to generate glitched image
-    // ...
-  
-    // Set glitched image source
-    image.src = "My-photo-for-the-socials-_1_-reverb.png";
-  });
-  
-  document.getElementById("email-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    var emailInput = document.getElementById("email-input").value;
-    
-    // Logic to send the glitched image to the specified email
-    // ...
-    
-    alert("Glitched image sent to " + emailInput);
-  });
-  
+  alert("Glitched image sent to " + emailInput);
+}
